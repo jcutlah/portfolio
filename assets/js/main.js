fc4125344d2942b1036c8b30099878a7b6b82e63
James Cutler <jcutler@hubspot.com>
2019-05-26 05:18:35 -0400

    added mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log(document.body.scrollTop);
     var header = document.querySelector('.header-container');
+    if (window.scrollY > 0) {
+        if (header.classList.contains('sticky')) {
+            // do nothing
+        } else {
+            header.classList.add('sticky');
+        }
+    } else {
+        header.classList.remove("sticky");
+    }
     window.addEventListener("scroll", function(){
         // console.log(window.scrollY);
         if (window.scrollY > 0) {
@@ -14,4 +23,14 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         
     })
+    var menu = document.querySelector('ul.header-menu');
+    var mobileTrigger = document.querySelector('#mobile-trigger');
+    var body = document.querySelector('body');
+    mobileTrigger.addEventListener('click',function(){
+        if (body.className.indexOf('mobile-menu') > -1){
+            body.classList.remove('mobile-menu');
+        } else {
+            body.classList.add('mobile-menu');
+        }
+    });
 });
